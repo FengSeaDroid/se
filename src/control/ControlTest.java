a58fd7140b26b5354f89dae5255d7a3c4379ad6e
Feng Wu <fw1714@mun.ca>
2013-11-13 17:54:35 -0330

    mainControl adapted to set and added test case

---
+++ src/control/ControlTest.java
@@ -0,0 +1,27 @@
+package control;
+
+import static org.junit.Assert.*;
+
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import domain.Patient;
+import domain.Prescription;
+
+public class ControlTest {
+
+	@Before
+	public void setUp() throws Exception {
+		MainControl.getMainControl().lookupPatient("12345");
+	}
+
+	@Test
+	public void test() {
+		assertTrue(MainControl.getMainControl().getCurrentPatient() instanceof Patient);
+		assertEquals(MainControl.getMainControl().getCurrentPatient().getPrescriptionHistory().size(),2);
+		assertEquals(MainControl.getMainControl().getCurrentPatient().getPrescriptionHistory().iterator().next().getDrugLines().size(),4);
+	}
+
+}
