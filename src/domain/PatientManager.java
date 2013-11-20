6816a3b62e70ef5304e7b1a934e61b4e2fe76254
safwan <safwan@192.168.2.15>
2013-11-19 23:22:29 -0330

    adding getSignature method in the patient manager, and split code in patient manager class

--- src/domain/PatientManager.java
+++ src/domain/PatientManager.java
@@ -1,47 +1,65 @@
 package domain;
+import java.awt.Image;
 import java.sql.ResultSet;
 import java.util.HashSet;
 import java.util.Set;
+
+import javax.swing.JLabel;
 
 import control.MainControl;
 import mySqlDatabase.DBConnection;
 import domain.Patient;
 import domain.Prescription;
 
+/**
+ * A singleton Manager
+ *
+ */
 public class PatientManager {
 	
 	private static PatientManager patientManagerInstance = new PatientManager();
 	private DBConnection dbconnection;
 	private Patient patient;
 	
+	/**
+	 * Patient manager constructor
+	 */
 	private PatientManager() {
 		try 
 		{
+			//Connection to University DB server
 			dbconnection=new DBConnection("jdbc:mysql://mysql.cs.mun.ca:3306/cs6713","cs6713","3176sc!");
+			//local connection to my DB server
+			//dbconnection=new DBConnection("jdbc:mysql://localhost:3306/prescriptionsys","root","System");
 		}
 		catch ( Exception e ) {
 	    System.out.println(e.getMessage());
 		}
 	}
 	
+	/**
+	 * @return patient manager to be used whenever needed
+	 */
+
 	public static PatientManager getPatientManager(){
 		return patientManagerInstance;
 	}
 	
+	/**
+	 * search patient on the database based on his MCP and return patient object
+	 */
 	public Patient lookupPatient(String MCP){
 		try{
-			System.out.println("not enter quesry statement");
-			System.out.println(MCP);
+			System.out.println("MCP number is"+MCP);
 			ResultSet patientResult=dbconnection.execQuery("select patient_id,MCP,name,address,dob,"
 					+ "weight,phone from patient where MCp="+MCP);
-			System.out.println("affter second stat quesry statement");
 			
 			patientResult.next();
 			
 			String p_id=patientResult.getString("patient_id");
-			System.out.println(p_id);
-			//patient_id	MCP	name	address	dob	weight	phone
-
+			System.out.println("Patient ID"+p_id);
+			
+			//Patient table description in the DB (patient_id,MCP,name,address,dob,weight,phone)
 
 			String mcp=patientResult.getString("MCP");
 
@@ -55,54 +73,15 @@ public class PatientManager {
 
 			String tel=patientResult.getString("phone");
 			
-			System.out.println("DDDDDDDdone");
-
-			//patient_id	allergy_agent
-			ResultSet allergyResult=dbconnection.execQuery("select * from allergy where patient_id="+p_id);
-			System.out.println("DDDDDDDdone11111");
-
+			ResultSet allergyResult=getAlergy(p_id);
 			Set<String> allergySet = new HashSet<String>();
-			System.out.println("DDDDDDDdone22222");
 
 	        while (allergyResult.next()){
-				System.out.println("inside while");
-
+	        	
 	        	allergySet.add(""+allergyResult.getString("allergy_agent"));
 			}
-	       // patient.setAllergy(allergySet);
-			System.out.println("DDDDDDDdoneqqq");
-			
-			//prescription_id	issue_date	effective_date	physician_id	patient_id
-	        ResultSet prescriptionResult=dbconnection.execQuery("select ph.name,pr.prescription_id,pr.issue_date,pr.effective_date from prescription pr,physician ph"+
-			" where pr.patient_id="+p_id+" and pr.physician_id=ph.physician_id");
-	        
-	        //prescription_id	medicine_name	medicine_spec
-	        //ResultSet prescriptionSpecResult=dbconnection.execQuery("select medicine_name,medicine_spec from prescription_spec where "
-	        	//	+ "prescription_id=1");
-			
-			Set<Prescription> prescriptionSet=new HashSet<Prescription>();
-			
-	        while (prescriptionResult.next()){
-	        	
-	        	System.out.println("second while");
-	        	Set<String> drugLine=new HashSet<String>();
-        		String presc_id=prescriptionResult.getString("prescription_id");
-        		
-        		ResultSet prescriptionSpecResult=dbconnection.execQuery("select medicine_name,medicine_spec from prescription_spec where "
-    	        		+ "prescription_id="+presc_id);
-
-	        	while(prescriptionSpecResult.next())
-	        	{
-		        	System.out.println("third while");
-	        		drugLine.add(prescriptionSpecResult.getString("medicine_name")+" "+prescriptionSpecResult.getString("medicine_spec")); 
-	        	}
-        		Prescription prescription=new Prescription(prescriptionResult.getString(1),prescriptionResult.getString("issue_date")
-        				,prescriptionResult.getString("effective_date"),drugLine,false);
-        		System.out.println("done from third while");
-        		prescriptionSet.add(prescription);
-
-			}
-	        System.out.println("end while");
+	        Set<Prescription> prescriptionSet=new HashSet<Prescription>();
+	        prescriptionSet.addAll(this.getPrescriptionHistory(p_id));
 			patient=new Patient(p_id,name,mcp,dateOfBirth,weight,address,tel,allergySet,prescriptionSet);
 
 		}
@@ -112,12 +91,67 @@ public class PatientManager {
 		return patient;
 	}
 	
-	//medicine	medicine_spec
+	/**
+	 * Get allergy as ResultSet for particular patient
+	 */
+	public ResultSet getAlergy(String patientID){
+		try{
+			ResultSet patientAllergyList=dbconnection.execQuery("select * from allergy where patient_id="+patientID);
+			return patientAllergyList;
+
+		}
+		catch (Exception e){
+			System.out.println(e.getMessage());
+		}
+		return null;
+	}
+	
+	/**
+	 * Get prescription History for specific patient as set of prescription objects
+	 */
+	public Set<Prescription> getPrescriptionHistory(String patientID){
+		try{
+
+			ResultSet prescriptionResult=dbconnection.execQuery("select ph.name,pr.prescription_id,pr.issue_date,pr.effective_date from prescription pr,physician ph"+
+					" where pr.patient_id="+patientID+" and pr.physician_id=ph.physician_id");
+
+			Set<Prescription> prescriptionSet=new HashSet<Prescription>();
+
+			while (prescriptionResult.next()){
+
+				Set<String> drugLine=new HashSet<String>();
+				String presc_id=prescriptionResult.getString("prescription_id");
+
+				//Prescription_spec table description in the DB
+				ResultSet prescriptionSpecResult=dbconnection.execQuery("select medicine_name,medicine_spec from prescription_spec where "
+						+ "prescription_id="+presc_id);
+
+				while(prescriptionSpecResult.next())
+				{
+					drugLine.add(prescriptionSpecResult.getString("medicine_name")+" "+prescriptionSpecResult.getString("medicine_spec")); 
+				}
+				Prescription prescription=new Prescription(prescriptionResult.getString(1),prescriptionResult.getString("issue_date")
+						,prescriptionResult.getString("effective_date"),drugLine,false);
+
+				prescriptionSet.add(prescription);
+
+			}
+			return prescriptionSet;
+		}
+		catch (Exception e){
+			System.out.println(e.getMessage());
+		}
+		return null;
+	}
+	
+	/**
+	 * query the formulary table in the DB and return formulary object
+	 */
 	public Formulary lookupFormulary(){
 		Set<String> formularySet=new HashSet<String>();
 
 		try{
-
+			//formulary table description in the DB (medicine,medicine_spec)
 			ResultSet formularyResult=dbconnection.execQuery("select medicine,medicine_spec from formulary");
 			
 			while(formularyResult.next()){
@@ -131,6 +165,9 @@ public class PatientManager {
 		return formulary;
 	}
 	
+	/**
+	 * save prescription for specific user and add it to the database
+	 */
 	public void savePrescription(Prescription prescription){
 		try{
 			
@@ -152,8 +189,26 @@ public class PatientManager {
 		catch (Exception e){
 			System.out.println(e.getMessage());
 		}
-		//INSERT INTO COMMENTS values (default, 'lars', 'myemail@gmail.com','http://www.vogella.com', '2009-09-14 10:33:11', 'Summary','My first comment'); 
-
+	}
+	
+	public void getSignature(String physicianID)
+	{
+		try
+		{
+			Image image;
+			byte[] imageBytes;
+			ResultSet signatureResult=dbconnection.execQuery("select signature from physician where physician_id="+physicianID);
+			signatureResult.next();
+			imageBytes=signatureResult.getBytes("columnLabel");
+			//image=getToolkit().creatImage(imageBytes);
+			//ImageIcon icon=new Icon(image);
+			//JLabel x;
+			//x.setIcon(icon);
+		}
+		catch (Exception e){
+			System.out.println(e.getMessage());
+		}
+		
 	}
 	
 }
