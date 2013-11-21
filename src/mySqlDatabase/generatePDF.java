2d7daa781561a6c7baa45e487866d368a1ad2269
safwan <safwan@134.153.9.5>
2013-11-21 14:17:23 -0330

    Modifying PDF report by removing the table and show drug line without any boreder and adding the RX picture to the report

--- src/mySqlDatabase/generatePDF.java
+++ src/mySqlDatabase/generatePDF.java
@@ -1,10 +1,17 @@
 package mySqlDatabase;
 import java.awt.Desktop;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.Date;
 import java.util.Set;
-
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import com.itextpdf.text.Image;
 import com.itextpdf.text.Anchor;
 import com.itextpdf.text.BadElementException;
 import com.itextpdf.text.BaseColor;
@@ -21,20 +28,25 @@ import com.itextpdf.text.Section;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
-
+import com.itextpdf.text.pdf.codec.Base64.InputStream;
 import control.MainControl;
 import domain.Patient;
 import domain.Prescription;
 
-
+/**
+ * Class to generate the prescription report
+ * by calling one method named generateReport
+ * the libary used for generating PDF is Itext 
+ */
 public class generatePDF {
-  private static String FILE = "FirstPdf.pdf";
-  private static Font catFont = new Font(Font.FontFamily.TIMES_ROMAN, 18,
+  private static String FILE = "prescription.pdf";
+  private static Font titleFont = new Font(Font.FontFamily.TIMES_ROMAN, 16,
       Font.BOLD);
-  private static Font redFont = new Font(Font.FontFamily.TIMES_ROMAN, 12,
-      Font.NORMAL, BaseColor.RED);
-  private static Font subFont = new Font(Font.FontFamily.TIMES_ROMAN, 16,
-      Font.BOLD);
+  private static Font rxFont = new Font(Font.FontFamily.TIMES_ROMAN, 20,
+      Font.NORMAL, BaseColor.BLACK);
+  private static Font tableHeaderFont = new Font(Font.FontFamily.TIMES_ROMAN, 12);
+  private static Font tableFont = new Font(Font.FontFamily.TIMES_ROMAN, 12);
+
   private static Font smallBold = new Font(Font.FontFamily.TIMES_ROMAN, 12,
       Font.BOLD);
   private static Patient patient=MainControl.getMainControl().getCurrentPatient();
@@ -42,14 +54,21 @@ public class generatePDF {
   private static Document document = new Document();
   
 
-  public static void generateReport() throws Exception{
+  /**
+ * the only public method that can be called\
+ * to generate the PDF report
+ */
+public static void generateReport() throws Exception{
    
       PdfWriter.getInstance(document, new FileOutputStream(FILE));
       document.open();
       //addMetaData(document);
       generatePrescriptionInformarmation();
-      createTable();
+      //createTable();
+      creatRxImage();
+      createDrugLine();
       createSignature();
+      createDate();
       document.close();
       Desktop.getDesktop().open(new File(FILE));
    
@@ -66,13 +85,16 @@ public class generatePDF {
 //    document.addCreator("Lars Vogel");
 //  }
 
-  private static void generatePrescriptionInformarmation()
+  /**
+ * Used to generate the Prescription title and patient information
+ */
+private static void generatePrescriptionInformarmation()
       throws DocumentException {
     Paragraph prescriptionParagraph = new Paragraph();
     // We add one empty line
     addEmptyLine(prescriptionParagraph, 1);
     // Lets write a big header
-    Paragraph prescriptionTitle=new Paragraph("Gerard Farrell\n Clinic Name \n Clinic Address\n ClinicTell", catFont);
+    Paragraph prescriptionTitle=new Paragraph("Gerard Farrell\n Clinic Name \n Clinic Address\n ClinicTell", titleFont);
     addEmptyLine(prescriptionTitle, 2);
 
     prescriptionTitle.setAlignment(Element.ALIGN_CENTER);
@@ -82,53 +104,112 @@ public class generatePDF {
     // Start a new page
     //document.newPage();
     
-    Paragraph patientParagraph=new Paragraph("Name:"+patient.getName()+"                    MCP:"+
-    patient.getMCP()+"                      Address:"+patient.getAddress(), smallBold);
+    Paragraph patientParagraph=new Paragraph("Name:"+patient.getName()+"\nAddress:"+patient.getAddress()+
+    		"\nMCP:"+patient.getMCP()+"\nPhone:"+patient.getTel(), smallBold);
     addEmptyLine(patientParagraph, 1);
 
     document.add(patientParagraph);
-   
   }
   
-  private static void createTable() throws DocumentException {
+  /**
+ * Generate drug line information fot the patient
+ */
+private static void createDrugLine() throws DocumentException{
+	 
+	  Paragraph grugInformation=new Paragraph();
+	  addEmptyLine(grugInformation,1);
+	  for (String s : prescription.getDrugLines()) {
+	    	Paragraph drugline=new Paragraph(s);
+	    	drugline.setIndentationLeft(100);
+	    	grugInformation.add(drugline);
 
-   Paragraph tableParagraph=new Paragraph();
-    PdfPTable table = new PdfPTable(2);
+		  }
+	  addEmptyLine(grugInformation,1);
+	  document.add(grugInformation);
+  }
+  
+  /**
+ * @throws MalformedURLException
+ * @throws IOException
+ * @throws DocumentException
+ * Add the Rx image to the report
+ */
+private static void  creatRxImage() throws MalformedURLException, IOException, DocumentException{
+	  Image image = Image.getInstance("src/Rx.jpg");
+	  image.setAlignment(Element.ALIGN_LEFT);
+      //image.setAbsolutePosition(500f, 650f);
+      image.scaleAbsolute(100f, 75f);
+      document.add(image);
+  }
+  
+  /**
+ * @throws DocumentException
+ * create the drug line as table with 2 columns
+ * drug name and drug specefication
+ */
+private static void createTable() throws DocumentException {
 
-    // t.setBorderColor(BaseColor.GRAY);
-    // t.setPadding(4);
-    // t.setSpacing(4);
-    // t.setBorderWidth(1);
+	  Paragraph tableParagraph=new Paragraph();
+	  PdfPTable table = new PdfPTable(2);
 
-    PdfPCell c1 = new PdfPCell(new Phrase("Drug Name"));
-    c1.setHorizontalAlignment(Element.ALIGN_CENTER);
-    table.addCell(c1);
+	  PdfPCell c1 = new PdfPCell(new Phrase("Drug Name"));
+	  c1.setHorizontalAlignment(Element.ALIGN_CENTER);
+	  c1.setBorderColor(BaseColor.WHITE);
+	  table.addCell(c1);
 
-    c1 = new PdfPCell(new Phrase("Drug Specification"));
-    c1.setHorizontalAlignment(Element.ALIGN_CENTER);
-    table.addCell(c1);
-    
-    table.setHeaderRows(1);
+	  c1 = new PdfPCell(new Phrase("Drug Specification"));
+	  c1.setHorizontalAlignment(Element.ALIGN_CENTER);
+	  table.addCell(c1);
 
-    for (String s : prescription.getDrugLines()) {
-		String[] medicine=s.split(" ");
-    	table.addCell(medicine[0]);
-    	String medicine_spec="";
-    	for(int i=1; i<medicine.length;i++)
-    	{
-    		medicine_spec=medicine_spec+" "+medicine[i];
-    	}
-        table.addCell(medicine_spec);
-    }
-    
-    tableParagraph.add(table);
-    document.add(tableParagraph);
+	  table.setHeaderRows(1);
+
+	  for (String s : prescription.getDrugLines()) {
+		  String[] medicine=s.split(" ");
+		  Phrase medicineCell = new Phrase(medicine[0].toString(), tableFont);
+		  table.addCell(medicineCell);
+		  String medicine_spec="";
+		  for(int i=1; i<medicine.length;i++)
+		  {
+			  medicine_spec=medicine_spec+" "+medicine[i];
+		  }
+		  Phrase medicineSpecCell = new Phrase(medicine_spec.toString(), tableFont);
+		  table.addCell(medicineSpecCell);
+	  }
+	  tableParagraph.add(table);
+	  document.add(tableParagraph);
   }
 
-  private static void createSignature() throws DocumentException{
-	  Paragraph dateSignature=new Paragraph("Date: "+MainControl.getMainControl().getPrescription().getEffectiveDate());
-	  document.add(dateSignature);
+  /**
+ * @throws DocumentException
+ * @throws IOException
+ * get the signature image from the database for the current physician 
+ * and add it to the report
+ */
+private static void createSignature() throws DocumentException, IOException{
+	  ByteArrayInputStream in = new ByteArrayInputStream(MainControl.getMainControl().getPatientManager().getSignature(MainControl.getMainControl().getPhysicianID()));
+	  BufferedImage bImageFromConvert = ImageIO.read(in);
+	  System.out.println("Before creating image file");
+	  ImageIO.write(bImageFromConvert, "jpg", new File("text.jpg"));
+	  //String.format("D:\","text.jpg");
+	  System.out.println("after creating image file");
+	  Image image = Image.getInstance("text.jpg");
+	  image.setAlignment(Element.ALIGN_RIGHT);
+      //image.setAbsolutePosition(500f, 650f);
+      image.scaleAbsolute(100f, 50f);
+
+      document.add(image);
   }
+  
+  /**
+ * @throws DocumentException
+ * adding the effective date to the PDF report
+ */
+private static void createDate() throws DocumentException{
+	  Paragraph issueDate=new Paragraph("Date: "+MainControl.getMainControl().getPrescription().getEffectiveDate(),tableFont);
+	  issueDate.setAlignment(Element.ALIGN_RIGHT);
+	  document.add(issueDate);
+  }
+
   private static void addEmptyLine(Paragraph paragraph, int number) {
     for (int i = 0; i < number; i++) {
       paragraph.add(new Paragraph(" "));
