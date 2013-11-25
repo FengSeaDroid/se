c9900d5d59552b729d2020672aeaabcaa9efe160
Feng Wu <fw1714@mun.ca>
2013-11-25 13:15:15 -0330

    ignore button working

--- src/ui/AlertPopup.java
+++ src/ui/AlertPopup.java
@@ -1,23 +1,57 @@
 package ui;
 
 
+import java.awt.Component;
+import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.*;
 
 import net.miginfocom.swing.MigLayout;
 
-public class AlertPopup extends JFrame implements ActionListener{
+public class AlertPopup extends JFrame implements ActionListener, KeyListener{
 
 	public AlertPopup(String s){
-		this.add(new JLabel("Allergy alert: "+s));
+		super.setLayout(new MigLayout());
+		this.setSize(250,130);
+		this.setResizable(false);
+		this.setAlwaysOnTop(true);
+		this.setLocationRelativeTo(null);
+		this.add(new JLabel("Allergy alert: "+s),"wrap,center");
+		JButton delete = new JButton("Delete Drug");
+		JButton ignore = new JButton("Ignore");
+		ignore.addActionListener(this);
+		ignore.addKeyListener(this);
+		this.add(delete);
+		this.add(ignore);
 		this.pack();
 		this.setVisible(true);
+		delete.requestFocus();
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		dispose();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_ENTER ||e.getKeyCode() == KeyEvent.VK_SPACE){
+			dispose();
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
--- src/ui/LoginWindow.java
+++ src/ui/LoginWindow.java
@@ -1,12 +1,9 @@
 package ui;
 
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.sql.ResultSet;
 
 import javax.swing.JButton;
